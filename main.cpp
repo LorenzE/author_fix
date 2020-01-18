70a506371323fe0ef690fa717d264119abbdc377
LorenzE <lorenzesch@hotmail.com>
2020-01-18 12:50:11 -0500

    Fix qMakePair

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QFile file("C:/Git/author_fix/authorsLog.txt");
+    QFile file("../author_fix/authorsLog.txt");
     if(!file.open(QIODevice::ReadOnly)) {
         QMessageBox::information(0, "error", file.errorString());
     }
@@ -40,44 +40,44 @@ int main(int argc, char *argv[])
 
     QMap<QString,QPair<QString,QString>> authorInfos;
 
-    authorInfos["Christoph Dinh"] = qMakePair("Christoph Dinh", "");
-    authorInfos["chdinh"] = qMakePair("Christoph Dinh", "");
-    authorInfos["LorenzE"] = qMakePair("Lorenz Esch", "");
-    authorInfos["Lorenz Esch"] = qMakePair("Lorenz Esch", "");
-    authorInfos["lorenze"] = qMakePair("Lorenz Esch", "");
-    authorInfos["buildqa"] = qMakePair("Rob ", "");
-    authorInfos["Gabriel B Motta"] = qMakePair("Gabriel B Motta", "");
-    authorInfos["GBeret"] = qMakePair("", "");
-    authorInfos["LostSign"] = qMakePair("", "");
-    authorInfos["Felix Arndt"] = qMakePair("Felix Arndt", "");
-    authorInfos["Felix"] = qMakePair("Felix Arndt", "");
-    authorInfos["Limin Sun"] = qMakePair("Limin Sun", "");
-    authorInfos["liminsun"] = qMakePair("", "");
-    authorInfos["sheinke"] = qMakePair("Simon Heinke", "");
-    authorInfos["Viktor"] = qMakePair("Viktor Klueber", "");
-    authorInfos["ViktorKL"] = qMakePair("Viktor Klueber", "");
-    authorInfos["louis"] = qMakePair("Louis Eichhorst", "");
-    authorInfos["louiseichhorst"] = qMakePair("Louis Eichhorst", "");
-    authorInfos["Louis Eichhorst"] = qMakePair("Louis Eichhorst", "");
-    authorInfos["Erik Hornberger"] = qMakePair("Erik Hornberger", "");
-    authorInfos["juangpc"] = qMakePair("", "");
-    authorInfos["Chiran"] = qMakePair("", "");
-    authorInfos["Florian Schlembach"] = qMakePair("Florian Schlembach", "");
-    authorInfos["Seok Lew"] = qMakePair("Seok Lew", "");
-    authorInfos["Tim Kunze"] = qMakePair("Tim Kunze", "");
-    authorInfos["doerfelruben@aol.com"] = qMakePair("Ruben Doerfel", "");
-    authorInfos["Ruben Doerfel"] = qMakePair("Ruben Doerfel", "");
-    authorInfos["joewalter"] = qMakePair("Daniel Strohmeier", "");
-    authorInfos["rickytjen"] = qMakePair("Ricky Tjen", "");
-    authorInfos["SugandhaSachdeva"] = qMakePair("", "");
-    authorInfos["MKlamke"] = qMakePair("", "");
-    authorInfos["PetrosSimidyan"] = qMakePair("Petros Simidyan", "");
-    authorInfos["Petros Simidyan"] = qMakePair("Petros Simidyan", "");
-    authorInfos["Blerta Hamzallari"] = qMakePair("Blerta Hamzallari", "");
-    authorInfos["1Dice"] = qMakePair("Lars Debor", "");
-    authorInfos["1DIce"] = qMakePair("Lars Debor", "");
-    authorInfos["mfarisyahya"] = qMakePair("Faris Yahya", "");
-    authorInfos["Jana Kiesel"] = qMakePair("Jana Kiesel", "");
+    authorInfos["Christoph Dinh"] = qMakePair(QString("Christoph Dinh"), QString(""));
+//    authorInfos["chdinh"] = qMakePair("Christoph Dinh", "");
+//    authorInfos["LorenzE"] = qMakePair("Lorenz Esch", "");
+//    authorInfos["Lorenz Esch"] = qMakePair("Lorenz Esch", "");
+//    authorInfos["lorenze"] = qMakePair("Lorenz Esch", "");
+//    authorInfos["buildqa"] = qMakePair("Rob ", "");
+//    authorInfos["Gabriel B Motta"] = qMakePair("Gabriel B Motta", "");
+//    authorInfos["GBeret"] = qMakePair("", "");
+//    authorInfos["LostSign"] = qMakePair("", "");
+//    authorInfos["Felix Arndt"] = qMakePair("Felix Arndt", "");
+//    authorInfos["Felix"] = qMakePair("Felix Arndt", "");
+//    authorInfos["Limin Sun"] = qMakePair("Limin Sun", "");
+//    authorInfos["liminsun"] = qMakePair("", "");
+//    authorInfos["sheinke"] = qMakePair("Simon Heinke", "");
+//    authorInfos["Viktor"] = qMakePair("Viktor Klueber", "");
+//    authorInfos["ViktorKL"] = qMakePair("Viktor Klueber", "");
+//    authorInfos["louis"] = qMakePair("Louis Eichhorst", "");
+//    authorInfos["louiseichhorst"] = qMakePair("Louis Eichhorst", "");
+//    authorInfos["Louis Eichhorst"] = qMakePair("Louis Eichhorst", "");
+//    authorInfos["Erik Hornberger"] = qMakePair("Erik Hornberger", "");
+//    authorInfos["juangpc"] = qMakePair("", "");
+//    authorInfos["Chiran"] = qMakePair("", "");
+//    authorInfos["Florian Schlembach"] = qMakePair("Florian Schlembach", "");
+//    authorInfos["Seok Lew"] = qMakePair("Seok Lew", "");
+//    authorInfos["Tim Kunze"] = qMakePair("Tim Kunze", "");
+//    authorInfos["doerfelruben@aol.com"] = qMakePair("Ruben Doerfel", "");
+//    authorInfos["Ruben Doerfel"] = qMakePair("Ruben Doerfel", "");
+//    authorInfos["joewalter"] = qMakePair("Daniel Strohmeier", "");
+//    authorInfos["rickytjen"] = qMakePair("Ricky Tjen", "");
+//    authorInfos["SugandhaSachdeva"] = qMakePair("", "");
+//    authorInfos["MKlamke"] = qMakePair("", "");
+//    authorInfos["PetrosSimidyan"] = qMakePair("Petros Simidyan", "");
+//    authorInfos["Petros Simidyan"] = qMakePair("Petros Simidyan", "");
+//    authorInfos["Blerta Hamzallari"] = qMakePair("Blerta Hamzallari", "");
+//    authorInfos["1Dice"] = qMakePair("Lars Debor", "");
+//    authorInfos["1DIce"] = qMakePair("Lars Debor", "");
+//    authorInfos["mfarisyahya"] = qMakePair("Faris Yahya", "");
+//    authorInfos["Jana Kiesel"] = qMakePair("Jana Kiesel", "");
 
     return a.exec();
 }
